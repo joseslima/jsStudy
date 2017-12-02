5d43cafa6eb9935ebc6f350f4b0003af4038ea8a
José Guilherme <guislima47@mail.com>
2017-12-01 23:04:41 -0200

    add checkLines function

--- jogodavelha_JQuery/game.js
+++ jogodavelha_JQuery/game.js
@@ -10,16 +10,12 @@ $(document).ready(function(){
 			alert("Preencha o nome dos jogadores");
 			return false;
 		}  
-
-
 		$("#player1_name").html($("#input_player1").val()); 
 		$("#player2_name").html($("#input_player2").val()); 
 
 		$("#main").hide();
 		$("#board").show();
 	})
-
-
 	$(".played").click(function(){
 		var flag= checkMat(this.id[0],this.id[2]);
 		
@@ -64,8 +60,7 @@ $(document).ready(function(){
 
 	function checkGame(){
 		var aux= 0;
-		aux= checkDiag();
-		
+		aux= checkDiag();	
 		if (aux != 0){
 
 			if(aux == 3){
@@ -73,7 +68,6 @@ $(document).ready(function(){
 			}else{
 				alert("2 ganhou")
 			}
-
 		}else{
 			aux = checkLines();
 			if (aux != 0){
@@ -83,12 +77,22 @@ $(document).ready(function(){
 				}else{
 					alert("2 ganhou");
 				}
+			}else{
+				aux = checkColuns();
+				if (aux != 0){
+
+					if (aux == 3){
+						alert("1 ganhou");
+					}else{
+						alert("2 ganhou");
+					}
+				}
 			}
-
-
 		}
 	}
-
+	function checkColuns(){
+		return 0;
+	}
 	function checkLines(){
 		var aux= 0;
 		for (var i = 0; i < global_mat.length; i++) {
@@ -100,12 +104,8 @@ $(document).ready(function(){
 			}
 			aux = 0;
 		}
-
 		return aux;
 	}
-
-
-
 	function checkDiag(){
 		if (  (global_mat[0][0] + global_mat[1][1] + global_mat[2][2]) == 3 || (global_mat[0][2] + global_mat[1][1] + global_mat[2][0]) == 3 ) {
 			return 3;
